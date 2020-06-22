db7605f6cb09ec1d8df4cf86bbf4e47a3951257f
Yoni Gibbs <39593145+yonigibbs@users.noreply.github.com>
2020-06-22 09:45:01 +0100

    Initial sample

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-import "./styles.css";
+function generateSum() {
+  let x = Math.floor(Math.random() * 100);
+  let y = Math.floor(Math.random() * 100);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  document.getElementById("first").innerText = Math.max(x, y);
+  document.getElementById("second").innerText = Math.min(x, y);
+}
